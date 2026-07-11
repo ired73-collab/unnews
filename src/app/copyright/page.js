a4b8ee62c1257b5045ca67d4c7a80486bb9e8728
MIHWA KOO <abc@gmail.com>
2026-07-12 00:06:12 +0900

    fix: add policy routes

---
+++ src/app/copyright/page.js
@@ -0,0 +1,5 @@
+import MainPage from "../page";
+
+export default function CopyrightPage() {
+  return <MainPage />;
+}
---
+++ src/app/privacy/page.js
@@ -0,0 +1,5 @@
+import MainPage from "../page";
+
+export default function PrivacyPage() {
+  return <MainPage />;
+}
