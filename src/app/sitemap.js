12271cf27823525c960b828f9691b045842e991d
MIHWA KOO <abc@gmail.com>
2026-07-28 16:27:51 +0900

    공식 도메인 및 이미지 주소 수정

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -10,7 +10,7 @@ function createSlug(text = "") {
 }
 
 export default async function sitemap() {
-  const baseUrl = "https://unnews.vercel.app";
+  const baseUrl = "https://unnews.co.kr/";
 
   const { data } = await supabase
     .from("posts")
